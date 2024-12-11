906e542b54b43b1ec3ff9af091375cf4d93c7fb7
Pomah_Nrhotab <garash96_2@mail.ru>
2024-12-12 00:27:57 +0300

    1

--- controllers/userController.js
+++ controllers/userController.js
@@ -66,6 +66,7 @@ class UserController {
   async refresh(req, res, next) {
     try {
       const { refreshToken } = req.cookies;
+      console.log(refreshToken);
       const userData = await userService.refresh(refreshToken);
       res.cookie("refreshToken", userData.refreshToken, {
         maxAge: 30 * 24 * 60 * 60 * 1000,
