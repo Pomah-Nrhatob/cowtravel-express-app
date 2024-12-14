2689850eea20e32a116ad1618d8b51a5b1afa681
Pomah_Nrhotab <garash96_2@mail.ru>
2024-12-14 20:28:40 +0300

    1

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: "http://cowtravel.ru",
+    origin: "https://cowtravel.ru",
   })
 );
 app.use(express.json({ extend: true }));
